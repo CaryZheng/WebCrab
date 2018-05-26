1ea8236772df392c800336f8cde32b16cde59927
CaryZheng <CaryZhengMy@gmail.com>
2018-05-26 21:46:35 +0800

    Init project

---
+++ src/main/java/com/zzb/webcrab/WebcrabApplication.java
@@ -0,0 +1,12 @@
+package com.zzb.webcrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcrabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcrabApplication.class, args);
+	}
+}
